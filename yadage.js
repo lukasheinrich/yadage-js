7f22c67484c2567f8557495ca476b1c29b69221b
lukasheinrich <lukas.heinrich@gmail.com>
2017-12-02 20:26:42 +0100

    initial JavaScript

---
+++ yadage.js
@@ -0,0 +1,10 @@
+define(["jquery","cytoscape","cytoscape-dagre","dagre","cytoscape-expand-collapse"],function(
+  $,
+  cytoscape,
+  cytoscape_dagre,
+  dagre,
+  cytoscape_expand_collapse
+){
+  initialize_graph: function(){console.log("Hello World!")}
+})
+
