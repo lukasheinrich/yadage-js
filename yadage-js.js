e3c31f79bba8ba69e05973776476c8743e896d60
lukasheinrich <lukas.heinrich@gmail.com>
2017-12-02 20:42:08 +0100

    dummy release

---
+++ yadage-js.js
@@ -0,0 +1,11 @@
+define(["jquery","cytoscape","cytoscape-dagre","dagre","cytoscape-expand-collapse"],function(
+  $,
+  cytoscape,
+  cytoscape_dagre,
+  dagre,
+  cytoscape_expand_collapse
+){
+  return {
+    initialize_graph: function(){console.log("Hello World!")}
+  }
+})
